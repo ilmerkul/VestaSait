8b410f3eb6fca1d1343d02c2afdce8eb70a2f366
pikcsay <pikcsay@gmail.com>
2021-10-29 10:34:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active')
 		$('body').toggleClass('lock')
+	});
+	$('li.phone .phone__btn').click(function(event) {
+		$('.sub-phone,li.phone').toggleClass('active')
 	})
 });

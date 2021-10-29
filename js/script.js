4e34b48f3b49a4e09e6e929cbb97fdcd8b91d140
pikcsay <pikcsay@gmail.com>
2021-10-29 21:34:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,5 +5,48 @@ $(document).ready(function() {
 	});
 	$('li.phone .phone__btn').click(function(event) {
 		$('.sub-phone,li.phone').toggleClass('active')
+	});
+	$('.tab1').click(function(event) {
+		if ($('.tab2').hasClass('active')) {
+			$('.tab2').removeClass('active')
+			$('.tab1').addClass('active')
+		}
+	});
+	$('.tab2').click(function(event) {
+		if ($('.tab1').hasClass('active')) {
+			$('.tab1').removeClass('active')
+			$('.tab2').addClass('active')
+		}
 	})
 });
+
+
+
+ClassBtnSelect1 = '.deals'
+ClassListSelect1 = '.deals__list'
+ClassListSelectItem1 = '.deals__list-item'
+ClassInputSelect1 = '.deal__input'
+
+function CustomSelect(ClassBtnSelect, ClassListSelect, ClassListSelectItem, ClassInputSelect) {
+	document.querySelector(ClassBtnSelect).addEventListener('click', function () {
+	document.querySelector(ClassListSelect).classList.toggle('visible')
+	});
+
+
+	document.querySelectorAll(ClassListSelectItem).forEach(function (listItem) {
+		listItem.addEventListener('click', function (e) {
+			e.stopPropagation();
+			document.querySelector(ClassBtnSelect).innerText = this.innerText;
+			document.querySelector(ClassInputSelect).value = this.dataset.value;
+			document.querySelector(ClassListSelect).classList.remove('visible');
+		})
+	});
+
+	document.addEventListener('click', function (e) {
+		if (e.target !== document.querySelector(ClassBtnSelect)) {
+			document.querySelector(ClassListSelect).classList.remove('visible');
+		}
+	})
+};
+
+CustomSelect(ClassBtnSelect1, ClassListSelect1, ClassListSelectItem1, ClassListSelectItem1);
